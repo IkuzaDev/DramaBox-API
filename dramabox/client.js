9d73b3b5ecfaf632fb221561a8442e6b128ee3a9
ikuza <97811292+IkuzaDev@users.noreply.github.com>
2025-10-02 04:11:53 +0700

    Add files via upload

---
+++ dramabox/client.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { getHeaders } from "./dramaboxHelper.js";
+
+// Helper umum untuk request API DramaBox
+export const apiRequest = async (endpoint, payload = {}, isDb2 = false, method = "post") => {
+    try {
+        if(isDb2){
+            const url = `https://www.webfic.com${endpoint}`;
+            const { data } = await axios[method](url, payload, {
+                headers: {
+                    "Content-Type": "application/json",
+                    "pline": "DRAMABOX",
+                    "language": "in",
+                }
+            });
+            return data;
+        }
+        const headers = await getHeaders();
+        const url = `https://sapi.dramaboxdb.com${endpoint}`;
+        const { data } = await axios[method](url, payload, { headers });
+        return data;
+    } catch (err) {
+        if (err.response) {
+            console.error(`❌ API Error [${endpoint}] →`, err.response.data);
+        } else {
+            console.error(`❌ Request Error [${endpoint}] →`, err.message);
+        }
+        throw err; // biar bisa ditangkap di caller
+    }
+};
---
+++ dramabox/dramaboxHelper.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+export const getToken = async () => {
+  try {
+    const url = "https://sapi.dramaboxdb.com/drama-box/ap001/bootstrap";
+    const deviceId = uuidv4(35);
+    const headers = {
+      "User-Agent": "okhttp/4.10.0",
+      "Accept-Encoding": "gzip",
+      "Content-Type": "application/json",
+      "tn": "",
+      "version": "430",
+      "vn": "4.3.0",
+      "cid": "DRA1000042",
+      "package-name": "com.storymatrix.drama",
+      "apn": "1",
+      "device-id": deviceId,
+      "language": "in",
+      "current-language": "in",
+      "p": "43",
+      "time-zone": "+0800",
+      "content-type": "application/json; charset=UTF-8"
+    };
+    
+    const data = {
+      distinctId: ""
+    };
+    
+    const res = await axios.post(url, data, { headers })
+
+    const tokenData = {
+      token: res.data.data.user.token,
+      deviceId,
+      timestamp: Date.now(),
+    };
+
+    return tokenData;
+  } catch (error) {
+    console.error("[ERROR] Gagal mengambil token:", error.message);
+    throw error;
+  }
+};
+
+/**
+ * Generate headers lengkap siap pakai
+ */
+export const getHeaders = async () => {
+  const { token, deviceId } = await getToken();
+
+  return {
+    "Host": "sapi.dramaboxdb.com",
+    "Tn": `Bearer ${token}`,
+    "Version": "430",
+    "Vn": "4.3.0",
+    "Cid": "DALPF1057826",
+    "Package-Name": "com.storymatrix.drama",
+    "Apn": "1",
+    "Device-Id": `${deviceId}`,
+    "device-id": deviceId,
+    "Language": "in",
+    "Current-Language": "in",
+    "P": "43",
+    "Time-Zone": "+0800",
+    "md": "Redmi Note 8",
+    "Ov": "14",
+    "Mf": "XIAOMI",
+    "Brand": "Xiaomi",
+    "Content-Type": "application/json; charset=UTF-8",
+    "User-Agent": "okhttp/4.10.0",
+  };
+};
+
+export default { getToken, getHeaders };
